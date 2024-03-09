fcfd94fd723e1589979d244df0fc56f09bd255f4
Pi͢͢͢kสcђu <146358318+IR-O@users.noreply.github.com>
2024-03-09 23:45:24 +0530

    Update config.py

--- config.py
+++ config.py
@@ -33,7 +33,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://t.me/iro_x_support",
+    "https://github.com/IR-O/Irone",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
