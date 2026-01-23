d944b8a921d7ea7ed02c69b58cf92f3078211064
Pi͢͢͢kสcђu <146358318+IR-O@users.noreply.github.com>
2026-01-23 08:01:02 +0530

    Update config.py

--- config.py
+++ config.py
@@ -14,7 +14,7 @@ API_HASH = getenv("API_HASH", "9f4889cd2437d72ede20428c07a909be")
 BOT_TOKEN = getenv("BOT_TOKEN", "5663640542:AAF0CiZp8T27_4YP1EW-jfutGNzjB5Xylo4")
 
 # Get your mongo url from cloud.mongodb.com
-MONGO_DB_URI = getenv("MONGO_DB_URI", "mongodb+srv://LUSTIFYXMUSIC:Abhi77394@lustifymusic.evxnqby.mongodb.net/?retryWrites=true&w=majority&appName=LUSTIFYMUSIC")
+MONGO_DB_URI = getenv("MONGO_DB_URI", "mongodb+srv://Irobot:pikkuopbot@cluster0.tccq3ld.mongodb.net/?appName=Cluster0")
 
 DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 9999999999999))
 SONG_DOWNLOAD_DURATION = int(getenv("SONG_DOWNLOAD_DURATION_LIMIT", 9999999999999))
